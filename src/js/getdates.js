57e81cc841f60480615d28989952575805eda717
ealbanca <ealbanca@byui.edu>
2025-07-21 12:56:53 -0600

    Updated Footer

---
+++ src/js/getdates.js
@@ -0,0 +1,3 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentYear").innerHTML = "<br><br>" + "Hared Albancando Robles<br><br>Final Project WDD 230<br><br>" + year + "&copy;";
