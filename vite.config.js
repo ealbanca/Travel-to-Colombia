95355a8f183065b55deeabb9d291ca28c3e9db14
ealbanca <ealbanca@byui.edu>
2025-07-22 22:38:39 -0600

    uodated vite.config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        package: resolve(__dirname, "src/package_pages/index.html"),
+        packageList: resolve(__dirname, "src/package_list/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+      },
+    },
+  },
+});
