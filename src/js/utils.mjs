75ef9c26761854d1f973b942bac996c740ce3ae8
ealbanca <ealbanca@byui.edu>
2025-07-22 20:40:51 -0600

    Added comments to the index main file

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -17,7 +17,6 @@ export async function renderWithTemplate(
 }
 
 function loadTemplate(path) {
-    // wait what?  we are returning a new function? this is called currying and can be very helpful.
     return async function () {
         const res = await fetch(path);
         if (res.ok) {
@@ -28,52 +27,22 @@ function loadTemplate(path) {
 }
 
 export async function loadHeaderFooter() {
-    // Detect if we're in a subdirectory by checking the current path
-    const isInSubfolder = window.location.pathname.includes('/package_list/') || 
-                         window.location.pathname.includes('/package_pages/');
-    const basePath = isInSubfolder ? '../' : './';
-    
     console.log('loadHeaderFooter - Current path:', window.location.pathname);
-    console.log('loadHeaderFooter - Is in subfolder:', isInSubfolder);
-    console.log('loadHeaderFooter - Base path:', basePath);
 
-    // header template will still be a function! But one where we have pre-supplied the argument.
-    const headerTemplateFn = loadTemplate(`${basePath}public/partials/header.html`);
-    const footerTemplateFn = loadTemplate(`${basePath}public/partials/footer.html`);
+    // Use absolute paths that work from any location
+    const headerTemplateFn = loadTemplate('/public/partials/header.html');
+    const footerTemplateFn = loadTemplate('/public/partials/footer.html');
     const headerEl = document.querySelector("#main-header");
     const footerEl = document.querySelector("#main-footer");
 
-    // Load and adjust paths in templates
+    // Load templates (they already use absolute paths, so no path fixing needed)
     const headerTemplate = await headerTemplateFn();
     const footerTemplate = await footerTemplateFn();
 
-    // Fix image and link paths for subfolders
-    const fixedHeaderTemplate = isInSubfolder ?
-        headerTemplate
-            .replace(/src="\.\/public\/images\//g, 'src="../public/images/')
-            .replace(/href="\.\/index\.html"/g, 'href="../index.html"')
-            .replace(/href="\.\/contact\.html"/g, 'href="../contact.html"')
-            .replace(/href="\.\/cart\.html"/g, 'href="../cart.html"')
-            .replace(/href="package_list\/index\.html"/g, function(match) {
-                // If we're in package_pages, go to ../package_list/index.html
-                // If we're in package_list, go to ./index.html
-                return window.location.pathname.includes('/package_pages/') ? 
-                    'href="../package_list/index.html' : 'href="./index.html';
-            })
-            .replace(/href="package_list\/index\.html\?city=/g, function(match) {
-                return window.location.pathname.includes('/package_pages/') ? 
-                    'href="../package_list/index.html?city=' : 'href="./index.html?city=';
-            })
-        : headerTemplate;
+    // Insert the templates directly
+    headerEl.innerHTML = headerTemplate;
+    footerEl.innerHTML = footerTemplate;
 
-    const fixedFooterTemplate = isInSubfolder ?
-        footerTemplate.replace(/src="public\/images\//g, 'src="../public/images/')
-        : footerTemplate;
-
-    // Insert the fixed templates
-    headerEl.innerHTML = fixedHeaderTemplate;
-    footerEl.innerHTML = fixedFooterTemplate;
-    
     // Set up footer copyright information
     setupFooter();
 }
