7a01a023803b9b26339eda8611f7066231c0e2e0
ealbanca <ealbanca@byui.edu>
2025-07-23 19:33:17 -0600

    Added notes to globalAuth file

--- src/js/globalAuth.js
+++ src/js/globalAuth.js
@@ -1,13 +1,8 @@
-/**
- * Global Authentication Modal Handler
- * Include this script on any page where you want the login modal functionality
- */
+
+//Global Authentication Modal Handler, to login and signup users from any page
 
 import { openLoginModal, closeLoginModal } from './utils.mjs';
 
 // Make modal functions globally available
 window.openLoginModal = openLoginModal;
 window.closeLoginModal = closeLoginModal;
-
-// Export for module usage
-export { openLoginModal, closeLoginModal };
